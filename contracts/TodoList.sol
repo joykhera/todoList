88311fb0ab416b27f27b3552275c6c934c7033df
joykhera <73572578+joykhera@users.noreply.github.com>
2022-01-25 00:02:08 -0500

    a

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,5 +1,23 @@
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList {
     uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public {
+        createTask("Check out dappuniversity.com");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
